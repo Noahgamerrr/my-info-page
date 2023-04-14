09adb87005519cf5414c3b03b0ce6a632f4ea547
Noahgamerrr <noah.arsic@gmx.at>
2023-04-14 20:22:29 +0200

    Added some styling/responsiveness to the navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -7,18 +8,37 @@ import Home from './pages/Home';
 import Technologies from './pages/Technologies';
 import Languages from './pages/Languages';
 import Error404 from './pages/Error404';
+import AboutMe from './pages/AboutMe';
 
 function App() {
+  const [windowSize, setWindowSize] = React.useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  React.useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
   return (
-    <div style={{height: "100vh"}}>
+    <div style={{height: windowSize[0] >= 768 && "calc(100vh - 46px)"}}>
       <BrowserRouter>
         <Header/>
         <div className="container-fluid h-100">
           <div className='row h-100'>
             <Navbar/>
-            <main className='col-md-10'>
+            <main className='col-md-10 ps-4 pt-2'>
               <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='about-me' element={<AboutMe/>}/>
                 <Route path='technologies' element={<Technologies/>}/>
                 <Route path='languages' element={<Languages/>}/>
                 <Route path='*' element={<Error404/>}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
-import { Outlet, Link } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
 
 export default function Navbar() {
+    const [active, setActive] = React.useState("home");
     return (
         <>
             <nav className="sidebar col-md-2 shadow">
                 <div className="list-group list-group-flush">
-                    <Link className="list-group-item" to="/">Home</Link>
-                    <Link className="list-group-item" to="/technologies">Technologies</Link>
-                    <Link className="list-group-item" to="/languages">Languages</Link>
+                    <Link className="list-group-item" to="/" onClick={() => setActive("home")}>
+                        <span className={active === "home" && "link-active"}>Home</span>
+                    </Link>
+                    <Link className="list-group-item" to="/about-me" onClick={() => setActive("about")}>
+                        <span className={active === "about" && "link-active"}>About me</span>
+                    </Link>
+                    <Link className="list-group-item" to="/technologies" onClick={() => setActive("technologies")}>
+                        <span className={active === "technologies" && "link-active"}>Technologies</span>
+                    </Link>
+                    <Link className="list-group-item" to="/languages" onClick={() => setActive("languages")}>
+                        <span className={active === "languages" && "link-active"}>Languages</span>
+                    </Link>
                 </div>
             </nav>
             <Outlet />
