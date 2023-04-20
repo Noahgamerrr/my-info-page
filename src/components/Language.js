a958756e8f2e43b626396a9963b2c1dedbe5affb
Noahgamerrr <noah.arsic@gmx.at>
2023-04-20 15:30:19 +0200

    Extracted the descriptions to their own text-files

--- src/components/Language.js
+++ src/components/Language.js
@@ -1,10 +1,21 @@
+import React from "react";
+
 export default function Language(props) {
+    const [desc, setDesc] = React.useState("");
+    const file = `/descriptions/${props.item.text}`;
+    console.log(file);
+    React.useEffect(() => {
+        fetch(file)
+            .then(t => t.text())
+            .then(text => setDesc(text));
+        }, [file]
+    );
     const textStyle = {textAlign: "justify"};
     return (
         <div className={`language-grid-${props.item.id % 2 + 1}`}>
             <h2 className={`language-title-${props.item.id % 2 + 1} text-center`}>{props.item.name}</h2>
             <img src={`./images/${props.item.img}`} alt="c" className={`language-image-${props.item.id % 2 + 1}`}></img>
-            <p style={textStyle} className={`language-description-${props.item.id % 2 + 1}`}>{props.item.text}</p>
+            <p style={textStyle} className={`language-description-${props.item.id % 2 + 1}`}>{desc}</p>
         </div>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,25 @@ import React from "react";
 export default function Navbar() {
     const [active, setActive] = React.useState("home");
     return (
-        <>
-            <nav className="sidebar col-md-2 shadow">
-                <div className="list-group list-group-flush">
-                    <Link className="list-group-item" to="/" onClick={() => setActive("home")}>
-                        <span className={active === "home" && "link-active"}>Home</span>
-                    </Link>
-                    <Link className="list-group-item" to="/about-me" onClick={() => setActive("about")}>
-                        <span className={active === "about" && "link-active"}>About me</span>
-                    </Link>
-                    <Link className="list-group-item" to="/technologies" onClick={() => setActive("technologies")}>
-                        <span className={active === "technologies" && "link-active"}>Technologies</span>
-                    </Link>
-                    <Link className="list-group-item" to="/languages" onClick={() => setActive("languages")}>
-                        <span className={active === "languages" && "link-active"}>Languages</span>
-                    </Link>
-                </div>
-            </nav>
+        <> 
+            <div className="sidebar col-md-2 shadow">
+                <nav className="position-md-fixed">
+                    <div className="list-group list-group-flush">
+                        <Link className="list-group-item" to="/" onClick={() => setActive("home")}>
+                            <span className={active === "home" ? "link-active" : undefined}>Home</span>
+                        </Link>
+                        <Link className="list-group-item" to="/about-me" onClick={() => setActive("about")}>
+                            <span className={active === "about" ? "link-active" : undefined}>About me</span>
+                        </Link>
+                        <Link className="list-group-item" to="/technologies" onClick={() => setActive("technologies")}>
+                            <span className={active === "technologies" ? "link-active" : undefined}>Technologies</span>
+                        </Link>
+                        <Link className="list-group-item" to="/languages" onClick={() => setActive("languages")}>
+                            <span className={active === "languages" ? "link-active" : undefined}>Languages</span>
+                        </Link>
+                    </div>
+                </nav>
+            </div>
             <Outlet />
         </>
     )
