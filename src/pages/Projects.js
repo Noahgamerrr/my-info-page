bef89102bd47be5bda8398b9633dddec46424c13
Noahgamerrr <noah.arsic@gmx.at>
2023-05-17 08:11:57 +0200

    Fixed an animation bug

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,38 @@
 import projects from "../data/projects.json"
 import Project from "../components/Project"
+import React from "react";
 
 export default function Projects() {
+    const [screenSize, setScreenSize] = React.useState(getCurrentDimension());
+
+  	function getCurrentDimension(){
+    	return {
+      		width: window.innerWidth,
+      		height: window.innerHeight
+    	}
+  	}
+  
+  	React.useEffect(() => {
+    		const updateDimension = () => {
+      			setScreenSize(getCurrentDimension())
+    		}
+    		window.addEventListener('resize', updateDimension);
+    
+		
+    		return(() => {
+        		window.removeEventListener('resize', updateDimension);
+    		})
+  	}, [screenSize]);
+
     let projectsColumnOne = [];
     let projectsColumnTwo = [];
+    let projectsMobile = [];
     let onFirstColumn = true;
     let projCount = 0;
     projects.forEach(p => {
         let projectElem = <Project order={projCount} key={p.id} item={p}/>;
         onFirstColumn ? projectsColumnOne.push(projectElem) : projectsColumnTwo.push(projectElem);
+        projectsMobile.push(projectElem);
         onFirstColumn = !onFirstColumn;
         projCount++;
     })
@@ -18,11 +42,13 @@ export default function Projects() {
             <div className="container w-75">
                 <div className="row">
                     <div className="col-md-6">
-                        {projectsColumnOne}
+                        {screenSize.width > 768 ? projectsColumnOne : projectsMobile}
                     </div>
-                    <div className="col-md-6">
-                        {projectsColumnTwo}
-                    </div>
+                    {   screenSize.width > 768 &&
+                        <div className="col-md-6">
+                            {projectsColumnTwo}
+                        </div>
+                    }
                 </div>
             </div>
         </>
