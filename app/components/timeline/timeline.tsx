d46f1a864f4f0a3c5d670e1535b1516cd54192d7
Noahgamerrr <noah.arsic@gmx.at>
2025-12-01 19:55:59 +0100

    finished timeline

--- app/components/timeline/timeline.tsx
+++ app/components/timeline/timeline.tsx
@@ -3,21 +3,28 @@ interface TimelineProps {
 }
 
 interface TimelineElementProps {
-    title: string
+    title: string,
+    isLast: boolean
 }
 
 export const Timeline = ({titles}: TimelineProps) => {
     return (
-        <div className="h-full">
-            {titles.map((title, idx) => <TimelineElement title={title} key={idx}/>)}
+        <div className="fixed w-1/8 h-3/4 flex flex-col justify-between top-1/2 -translate-y-1/2">
+            {titles.map((title, idx) => 
+                <TimelineElement 
+                    title={title} 
+                    isLast={idx == titles.length - 1} 
+                    key={idx}
+                />
+            )}
         </div>
     )
 }
 
-const TimelineElement = ({title}: TimelineElementProps) => {
+const TimelineElement = ({title, isLast}: TimelineElementProps) => {
     return (
-        <div className="flex flex-col mb-20">
-            <div className="after:absolute after:box-content after:h-5 after:w-5 after:bg-unfocused after:rounded-full after:translate-x-1/2"></div>
+        <div>
+            <div className={`after:absolute after:h-5 after:w-5 after:bg-unfocused after:rounded-full after:translate-x-1/2 before:w-1 before:h-1/3 before:bg-unfocused before:absolute before:left-5 before:-translate-x-1/2 before:translate-y-2 ${isLast && "before:hidden"}`}/>
             <div className="ms-10 font-bold text-unfocused">{title}</div>
         </div>
         
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"use client"
 import localFont from 'next/font/local';
 import { Timeline } from './components/timeline/timeline';
+import { RefObject, useEffect, useRef, useState } from 'react';
+import  { Element, scroller } from "react-scroll";
 
 const interFont = localFont({
     src: [
@@ -30,14 +33,50 @@ const gentiumFont = localFont({
 const titles = ["Work experience", "Technologies", "Projects", "About me"]
 
 export default function Home() {
-  return (
-    <main className={`min-h-screen w-full flex flex-row py-32 px-16 sm:items-start ${interFont.className}`}>
-        <Timeline titles={titles}/>
-        <div className='mx-auto'>
-            <p className={`text-9xl italic ${gentiumFont.className}`}>Noah Arsić</p>
-            <p className={`text-3xl italic ${gentiumFont.className}`}>est. 2005</p>
-            <p className="text-6xl mt-10">Passionate software developer</p>
-        </div>
-      </main>
+    const [scrollTop, setScrollTop] = useState(0);
+    const scrollToRef : RefObject<HTMLElement | null> = useRef(null);
+
+    const fontSize = scrollTop <= 68 ? 128 - scrollTop : 60 
+
+    
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (scrollTop < window.scrollY) scroller.scrollTo('test', {
+                duration: 40,
+                delay: 0,
+                smooth: true
+            })
+            setScrollTop(window.scrollY)
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [scrollTop]);
+
+    return (
+        <main 
+            className={`min-h-screen w-full py-32 px-16 sm:items-start ${interFont.className}`}
+        >
+            <Timeline titles={titles}/>
+            <div className='mt-30 w-7/8 ml-auto flex flex-col justify-center'>
+                <p 
+                    className={`italic sticky top-5 h-32 ${gentiumFont.className}`}
+                    style={{fontSize: fontSize, lineHeight: 1}}
+                >
+                    Noah Arsić
+                </p>
+                <p className={`text-3xl italic ${gentiumFont.className}`}>est. 2005</p>
+                <p className="text-6xl mt-10">Passionate software developer</p>
+                {/*
+                <Element name="test">
+                    <p>a</p>
+                </Element>
+                 */}
+            </div>
+        </main>
   );
 }
